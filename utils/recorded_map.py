71956634f134a642db9fbd1b41d4873cc29ab2f5
Nanjun <18906571516@163.com>
2023-07-24 22:31:29 +0800

    修改录制脚本

--- utils/recorded_map.py
+++ utils/recorded_map.py
@@ -4,6 +4,7 @@ import win32api
 import win32con
 import json
 
+from time import perf_counter
 from pynput import keyboard
 from pynput.keyboard import Key, KeyCode
 
@@ -12,84 +13,108 @@ from pynput.keyboard import Key, KeyCode
 w, a, s, d 移动角色
 x 角色攻击
 上下左右键是控制角色视角, 例如按 <- 或 -> 等价于鼠标向左或者向右移动
+f7暂停录制, f8关闭录制
 """
 
-is_stop = False
-key_board = ['w', 'a', 's', 'd', 'x', 'shift']  # 角色移动按键
-key_move_view = ['left', 'right', 'up', 'down']  # 视角按键 | 上下左右键控制角色视角
-key_list = []
-is_curren_key = None
+is_stop = False  # 暂停录制
+is_release = True  # 用来判断按键是否释放
+character_key = ['w', 'a', 's', 'd', 'x', 'shift']  # 角色移动按键
+character_view = ['left', 'right', 'up', 'down']  # 视角按键 | 上下左右键控制角色视角
+special_key = ['f7', 'f8']  # 特殊按键
+event_list = []  # 事件
+last_time = 0  # 键盘按压的时间
+key_dict = {}  # 按键信息 {key: time}
+filename = f"./map/world.json"  # 文件名
+
+
+def key_to_str(key):
+    """
+    将按键转化成字符串
+    """
+    if isinstance(key, KeyCode):
+        return key.char
+    elif isinstance(key, Key):
+        return key.name
+    else:
+        return ""
 
 
 def move_view(k: str):
-    k_to_int = {'left': (-500, 0),
-                'right': (500, 0),
-                'up': (0, -500),
-                'down': (0, 500)}
-    if k not in k_to_int:
+    """
+    移动视角
+    传入left就映射到鼠标向左移动
+    """
+    k_to_tuple = {'left': (-300, 0),
+                  'right': (300, 0),
+                  'up': (0, -300),
+                  'down': (0, 300)}
+    if k not in k_to_tuple:
         raise ValueError("请输入正确的方向")
-    x, y = k_to_int[k]
+    x, y = k_to_tuple[k]
     win32api.mouse_event(win32con.MOUSE_MOVED, x, y)  # 移动视角
 
 
 def on_press(key):
-    global is_stop, is_curren_key
-    if isinstance(key, Key) and key.name == 'f7':
+    global last_time, is_stop, is_release
+    key = key_to_str(key)
+    if is_release:
+        last_time = perf_counter()
+    # 跳过不监听的按键
+    if key not in character_key and key not in character_view and key not in special_key:
+        return None
+    # 暂停录制
+    if key == 'f7':
         is_stop = False if is_stop else True
         return None
+    # 退出录制
+    if key == 'f8':
+        return False
     if is_stop:
-        print("你已暂停录制请按f7恢复录制")
+        print("您已暂停录制，按f7继续录制")
         return None
 
-    if isinstance(key, Key):
-        key = key.name
-        # f8暂停录制
-        if key == 'f8':
-            return False
-        if key in key_move_view:
-            move_view(key)
-    elif isinstance(key, KeyCode):
-        key = key.char
+    if key in character_key:
         if key == 'x':
             pyautogui.click()
-    if key not in key_board and key not in key_move_view:
-        return None
-    # 同一个按键不重复记录，等释放
-    if key == is_curren_key and key not in key_move_view:
-        return None
-    elif key == is_curren_key and key in key_move_view:
-        key_list[-1][1] += 1
-        return None
-    # 移动
-    if key in key_board:
-        key_list.append([key, time.time()])
-    # 视角
-    elif key in key_move_view:
-        key_list.append([key, 1])
-    is_curren_key = key
+    elif key in character_view:
+        # 移动角色视角
+        move_view(key)
+        # 记录
+        event_list.append({'key': key, 'time': 0})
+    is_release = False
 
 
 def on_release(key):
-    global is_curren_key
-    if is_stop:
+    global is_release
+    key = key_to_str(key)
+    current_time = perf_counter()
+    # 跳过不监听的按键
+    if key not in character_key and key not in character_view and key not in special_key:
         return None
-    if isinstance(key, Key):
-        key = key.name
-    elif isinstance(key, KeyCode):
-        key = key.char
-    if key in key_board:
-        start_time = key_list[-1][1]
-        key_list[-1][1] = time.time() - start_time
-    is_curren_key = None
+    if is_stop:
+        print("您已暂停录制，按f7继续录制")
+        return None
+
+    if key in character_key:
+        event_list.append({'key': key, 'time': current_time - last_time})
+
+    is_release = True
 
 
-# 启动录制，f8关闭录制
-with keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release) as listener:
-    listener.join()
+def start():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
 
-print(key_list)
-map_name = "大世界"
-with open(fr'..\map\{map_name}.json', 'w') as f:
-    json.dump(key_list, f)
+
+if __name__ == '__main__':
+    print("录制将在2秒后开始")
+    time.sleep(2)
+    print("录制开始")
+    start()
+    json_dict = {"map_name": "",
+                 "number": "",
+                 "name": "NanJun",
+                 "keys": event_list}
+    print(event_list)
+    with open(filename, 'w') as f:
+        json.dump(json_dict, f)
