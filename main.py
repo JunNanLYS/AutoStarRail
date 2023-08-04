016fdee637c7e08f9a70a25601de5ae2394d2c9a
Nanjun <18906571516@163.com>
2023-08-04 15:18:32 +0800

    🔨

--- main.py
+++ main.py
@@ -9,6 +9,3 @@ window = MainWindow()
 window.resize(700, 600)
 window.show()
 sys.exit(app.exec())
-
-# pyinstaller --distpath . --workpath pack --specpath pack -i helpme.ico -F -w main.py
-# pyinstaller --hidden-import PySide6 -F -w main.py
--- script/world.py
+++ script/world.py
@@ -4,11 +4,7 @@ import json
 from pydantic import BaseModel
 
 from utils import tool
-from widgets import log
-from script.start_game import start_game
-from StarRailAssistant.run import run_map
 from StarRailAssistant.utils.config import sra_config_obj
-from StarRailAssistant.utils.update_file import update_file
 
 
 class MapData(BaseModel):
@@ -34,16 +30,6 @@ for json_file in json_files:
         m = MapData(author=data['author'], filename=json_filename, name=data['name'], number=map_number)
         name_to_map[data['name']] = m
         number_to_map[map_number] = m
-
-
-def run(map_name):
-    start_game()
-    log.transmitAllLog("运行锄大地")
-    os.chdir(r'F:\AutoStarRail\StarRailAssistant')
-    number = name_to_map[map_name].number  # 获取地图编号
-    run_map(number)
-    log.transmitAllLog('运行完成')
-
 
 update_dict = {
     "脚本": {
@@ -84,10 +70,3 @@ update_dict = {
     },
 }
 
-
-def update(target: str):
-    if target != "全部":
-        update_file().update_file_main(**update_dict[target])
-    else:
-        for update_data in list(update_dict.values()):
-            update_file().update_file_main(**update_data)
