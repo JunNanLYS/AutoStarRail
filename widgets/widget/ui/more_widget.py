cf63937a02059f83f1fbb0783d3e7ecba745449a
Nanjun <18906571516@163.com>
2023-08-06 17:32:41 +0800

    + 新功能
    一键执行所有脚本

--- widgets/widget/ui/more_widget.py
+++ widgets/widget/ui/more_widget.py
@@ -8,18 +8,13 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
+from PySide6.QtWidgets import (QHBoxLayout, QSizePolicy, QSpacerItem,
+                               QVBoxLayout, QWidget)
+from qfluentwidgets import (PlainTextEdit, PushButton, SmoothScrollArea)
 
 from ..card import Card
-from qfluentwidgets import (PushButton, SmoothScrollArea)
+
 
 class MoreWidgetUi(object):
     def setupUi(self, Form):
@@ -68,6 +63,25 @@ class MoreWidgetUi(object):
 
         self.verticalLayout.addWidget(self.mandateCard)
 
+        self.widget = Card(self.scrollAreaWidget)
+        self.widget.setObjectName(u"widget")
+        self.verticalLayout_2 = QVBoxLayout(self.widget)
+        self.verticalLayout_2.setSpacing(10)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(10, 10, 10, 10)
+        self.autoPlainTextEdit = PlainTextEdit(self.widget)
+        self.autoPlainTextEdit.setObjectName(u"autoPlainTextEdit")
+
+        self.verticalLayout_2.addWidget(self.autoPlainTextEdit)
+
+        self.autoButton = PushButton(self.widget)
+        self.autoButton.setObjectName(u"autoButton")
+
+        self.verticalLayout_2.addWidget(self.autoButton)
+
+
+        self.verticalLayout.addWidget(self.widget)
+
         self.verticalSpacer = QSpacerItem(20, 303, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
@@ -86,5 +100,10 @@ class MoreWidgetUi(object):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.commissionButton.setText(QCoreApplication.translate("Form", u"\u4e00\u952e\u9886\u53d6\u59d4\u6258", None))
         self.mandateButton.setText(QCoreApplication.translate("Form", u"\u5b8c\u6210\u6bcf\u65e5\u4efb\u52a1", None))
+        self.autoPlainTextEdit.setPlainText(QCoreApplication.translate("Form", u"\u4ec0\u4e48\u60c5\u51b5\u9002\u5408\u4f7f\u7528\u8be5\u529f\u80fd\uff1f\n"
+"1. \u60f3\u8981\u6240\u6709\u529f\u80fd\u4e00\u952e\u6267\u884c\u5230\u4f4d\n"
+"2. \u4e0d\u60f3\u81ea\u5df1\u50bb\u50bb\u7684\u7b49\u5f85\u7a0b\u5e8f\u6267\u884c\u7ed3\u675f\n"
+"3. ", None))
+        self.autoButton.setText(QCoreApplication.translate("Form", u"\u4e00\u952e\u5f00\u59cb\u81ea\u52a8", None))
     # retranslateUi
 
