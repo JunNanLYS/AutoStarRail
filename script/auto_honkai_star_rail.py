8ee694f44b2f62fc323d20b9f0f3d1b7ca9ab41d
Nanjun <18906571516@163.com>
2023-08-07 17:34:22 +0800

    新增一键运行所有功能按键

--- script/auto_honkai_star_rail.py
+++ script/auto_honkai_star_rail.py
@@ -1,16 +1,27 @@
 """
 自动运行所有脚本
 """
+from widgets import log
 from script import use_of_stamina, use_of_commission, use_of_world
 
 universe_count = 0  # 模拟宇宙计数
 
 
-def run(stamina_param, world_param):
+def run(stamina_param: dict, world_param: str):
+    """
+    传入参数时将按照参数来运行
+    未传入参数时将按照配置文件运行
+    配置文件为空时将不运行
+    """
     scripts = {
-        use_of_commission.run, (),
-        use_of_stamina.run, (stamina_param, ),
-        use_of_world.run, (world_param, ),
+        use_of_commission.run: (),
+        use_of_stamina.run: (stamina_param,),
+        use_of_world.run: (world_param,),
     }
-    for method, args in scripts:
+    log.transmitDebugLog(f"stamina_param: {stamina_param},  world_param: {world_param}", level=2)
+    for method, args in scripts.items():
         method(*args)
+
+
+if __name__ == "__main__":
+    run({"calyx_1": 1}, "大矿区-1")
