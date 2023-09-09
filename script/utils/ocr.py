8366d3f24c7069c737237cafbe6c8dceb552e8aa
Nanjun <18906571516@163.com>
2023-09-10 00:03:27 +0800

    cnocr改用ppocr

--- script/utils/ocr.py
+++ script/utils/ocr.py
@@ -1,45 +1,51 @@
-from cnocr import CnOcr
-
 import time
+
+from ppocronnx import TextSystem
+from numpy import ndarray, array
 
 import log
 
-cnocr = CnOcr()
+text_sys = TextSystem(use_angle_cls=False)
 
 
-def get_text_position(path, target: str):
+def get_text_position(img: ndarray, target: str) -> ndarray:
     """
-    获取文字坐标
+    获取文字坐标，没有查询到文字则返回空数组
     """
-    res = cnocr.ocr(path)
+    res = text_sys.detect_and_ocr(img)
     for i in range(len(res)):
-        d = res[i]
-        if target in d['text']:
-            return d['position']
-    return None
+        box = res[i]
+        if box.ocr_text == target:
+            return box.box
+    for i in range(len(res)):
+        box = res[i]
+        if target in box.ocr_text:
+            return box.box
+    return array([])
 
 
-def text_in_img(path, target: str) -> bool:
+def text_in_img(img: ndarray, target: str) -> bool:
     """
     判断图片中是否包含文字
     """
-    res = cnocr.ocr(path)
-    for i in range(len(res)):
-        d = res[i]
-        if target in d['text']:
-            return True
-    return False
+    res = get_text_position(img, target)
+    if res.size == 0:
+        return False
+    return True
 
 
 def wait_text(method, target: str, timeout: int = 60):
     """
+    :param method: 获取屏幕截图的方法，这个方法返回的必须是ndarray
+    :param target: 目标文字
+    :param timeout: 最长等待时长
     等待某个文字，若超过timeout时间未等到某个文字则抛出错误
     """
     start = time.time()
     while time.time() - start < timeout:
         pos = get_text_position(method(), target)
-        if pos is not None:
+        if pos.size != 0:
             return pos
-        time.sleep(0.1)
+        time.sleep(0.5)
     log.critical("等待超时")
     raise Exception("等待超时")
